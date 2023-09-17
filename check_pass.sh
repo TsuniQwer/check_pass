f610cf4ff4a8951aaa26f9d776c3a978a0157640
Sobol Roman <57962352+TsuniQwer@users.noreply.github.com>
2023-09-17 11:15:14 -0700

    Create check_pass.sh

---
+++ check_pass.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Пароль, который мы хотим проверить
+desired_password="HelloMyFrend#123"
+
+# Перебор всех пользователей в системе
+while IFS=: read -r username _ uid gid gecos home shell; do
+  # Установить пароль для текущего пользователя
+  echo "$username:$desired_password" | chpasswd
+  # Проверить, имеет ли пользователь доступ к оболочке Bash
+  if [[ "$shell" == *"/bash" ]]; then
+    echo "Пользователь $username имеет доступ к оболочке Bash с паролем '$desired_password'"
+  fi
+done < /etc/passwd
